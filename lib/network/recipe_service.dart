76688cdef93f3dd327c8add92f63309af19ed83d
허남규 <heonamgyu@heonamgyus-MacBook-Air.local>
2023-07-06 19:59:38 +0900

    for upload

---
+++ lib/network/recipe_service.dart
@@ -0,0 +1,38 @@
+import 'package:chopper/chopper.dart';
+import 'recipe_model.dart';
+import 'model_response.dart';
+import 'model_converter.dart';
+
+part 'recipe_service.chopper.dart';
+
+const String apiKey = 'abc23a15e93374ec2dad7112973e5676';
+const String apiId = 'bd5f638f';
+const String apiUrl = 'https://api.edamam.com';
+
+@ChopperApi()
+abstract class RecipeService extends ChopperService {
+  @Get(path: 'search')
+  Future<Response<Result<APIRecipeQuery>>> queryRecipes(
+      @Query('q') String query, @Query('from') int from, @Query('to') int to);
+
+  static RecipeService create() {
+    final client = ChopperClient(
+      baseUrl: Uri.parse(apiUrl),
+      interceptors: [_addQuery, HttpLoggingInterceptor()],
+      converter: ModelConverter(),
+      errorConverter: const JsonConverter(),
+      services: [
+        _$RecipeService(),
+      ],
+    );
+    return _$RecipeService(client);
+  }
+}
+
+Request _addQuery(Request req) {
+  final params = Map<String, dynamic>.from(req.parameters);
+  params['app_id'] = apiId;
+  params['app_key'] = apiKey;
+
+  return req.copyWith(parameters: params);
+}
